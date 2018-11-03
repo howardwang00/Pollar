c52a08b2da3cb30d38637cfab5cfdabe8edaa021
howardwang00 <howardwang00@gmail.com>
2018-11-03 13:58:53 -0700

    Connected App to Firebase

---
+++ pollar/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+var config = {
+  apiKey: "AIzaSyAV5-cCfX6W8muQX6kpybvd1U3fAnljiT4",
+  authDomain: "pollar-14859.firebaseapp.com",
+  databaseURL: "https://pollar-14859.firebaseio.com",
+  projectId: "pollar-14859",
+  storageBucket: "pollar-14859.appspot.com",
+  messagingSenderId: "662829354147"
+};
+firebase.initializeApp(config);
+export default firebase;
